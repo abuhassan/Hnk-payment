bab392a0d4b7d289a6ae6bcb8d095c8e92d49a12
Abu Hassan <abu.a.hassan@gmail.com>
2020-10-20 20:38:11 +0800

    bootstrap form random amount to pay

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,16 +5,29 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+                <div class="card-header">Make a payment</div>
 
                 <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
+                    <form action="#" method="POST" id="paymentForm">
+                        @csrf
+                        <div class="row">
+                            <div class="col-auto">
+                                <label>How much you want to pay</label>
+                                <input
+                                type="number"
+                                min="5"
+                                step="0.01"
+                                class="form-control"
+                                name="value"
+                                value="{{ mt_rand(500, 100000) / 100 }}"
+                                >
+                            </div>
                         </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
+                        <div class="text-center mt-3">
+                            <button type="submit" id="payButton" class="btn btn-primary
+                             btn-lg">Pay</button>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
