80223975087c5a7d5b2f53042a8bd40b356c0ff5
Abu Hassan <abu.a.hassan@gmail.com>
2020-10-21 21:33:57 +0800

    Actions to process payment

--- routes/web.php
+++ routes/web.php
@@ -11,11 +11,15 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
+ */
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::post('/payments/pay', 'App\Http\Controllers\PaymentController@pay')->name('pay');
+Route::get('/payments/approval', 'App\Http\Controllers\PaymentController@approval')->name('approval');
+Route::get('/payments/cancelled', 'App\Http\Controllers\PaymentController@cancelled')->name('cancelled');
 
 Auth::routes();
 
